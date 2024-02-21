9eb0b123045b82447b7b25951328195f9af46b4b
re-chart <145541584+re-chart@users.noreply.github.com>
2024-02-21 15:37:35 +0100

    Create FBRemediateScript.ps1

---
+++ PowerShell/Intune/Remediation/FastBoot/FBRemediateScript.ps1
@@ -0,0 +1,3 @@
+# Created by R. Visser -> www.richard-visser.nl
+# Turn Off FastBoot
+New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name 'HiberbootEnabled' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
