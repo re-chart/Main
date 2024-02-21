b6d9d23aa62e1fed11556f05461d69d7d3d13c56
re-chart <145541584+re-chart@users.noreply.github.com>
2024-02-21 09:43:43 +0100

    Update OfficeUpdate.ps1
    Now with Param option

--- PowerShell/OfficeUpdate.ps1
+++ PowerShell/OfficeUpdate.ps1
@@ -1,9 +1,14 @@
-﻿# Initial script created by B de Zeeuw
-# Script modified by R. Visser
-# Check and Update office if available.
+#############################################
+# Initial script created by B de Zeeuw
+# Modifications by R. Visser
 
-# Functie Sleep-Progress Bar
-# Use Sleep-Progress <seconds> 
+# Function for nice progressbar while waiting
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$Updateto
+)
+
 
 Function Sleep-Progress($TotalSeconds) {
     $Counter = 0;
@@ -14,72 +19,106 @@ Function Sleep-Progress($TotalSeconds) {
         $Counter++;
     }   
 }
- 
-# Start Office Version Check Script
 
-Write-Host "Checking script directory..." -ForegroundColor Green
-if (!(Test-Path "C:\Scripts")) {
-    Write-Host "Creating the C:\Scripts directory..." -ForegroundColor Green
-    New-Item -ItemType Directory -Path "C:\Scripts"
-}
-Write-Host "Script directory exists" -ForegroundColor Green
+if ([string]::IsNullOrEmpty($Updateto)) {
 
-Write-Host "Creating log file..." -ForegroundColor Green
-Get-Date | Out-File -FilePath "C:\Scripts\Office365Update.log"
-Add-Content -Path "C:\Scripts\Office365Update.log" -Value "Office 365 update process started" 
-$logfile = "C:\Scripts\Office365Update$((Get-Date).ToString('dd-MM-yyyy')).log"
-Write-Host "Log file created: $logfile" -ForegroundColor Green
-
-
-Write-Host "Starting the Office 365 update process..." -ForegroundColor DarkMagenta
-Add-Content -Path $logfile -Value "Starting the Office 365 update process..."
-
-#Check if the OfficeC2RClient.exe exists
-if (Test-Path "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe") {
-    Write-Host "OfficeC2RClient.exe exists"
-    Write-Host "Let's continue with the update process..." -ForegroundColor Green
-    Add-Content -Path $logfile -Value "OfficeC2RClient.exe exists"
-} else {
-    Write-Host "OfficeC2RClient.exe does not exist"
-    Add-Content -Path $logfile -Value "OfficeC2RClient.exe does not exist"
-    Write-Host "Exiting the update process..." -ForegroundColor Red
-    Add-Content -Path $logfile -Value "Exiting the update process..."
+Write-Host "############################################################################################################" -ForegroundColor Green
+Write-Host "Please provide the Office version to update to, example: scriptname.ps1 -updateto 16.0.17231.20236" -ForegroundColor Red
+Write-Host "############################################################################################################" -ForegroundColor Green
 
     exit 1
 }
 
+Write-Host "############################################################################################################" -ForegroundColor Green
+Write-Host "Checking script directory..." -ForegroundColor Green
 
-Add-Content -Path $logfile -Value "Checking the Office version..."
+if (!(Test-Path "C:\Scripts")) {
+    Write-Host "Creating the C:\Scripts directory..." -ForegroundColor Green
+    New-Item -ItemType Directory -Path "C:\Scripts"
+}
 
-Sleep-Progress 3
+Write-Host "Script directory exists" -ForegroundColor Green
+Write-Host "############################################################################################################" -ForegroundColor Green
+Write-Host "Creating log file..." -ForegroundColor Green
 
+Add-Content -Path "C:\Scripts\Office365Update.log" -Value "Office 365 update process started. .$(Get-Date -format "dd-MM-yyyy ss:mm:HH"))" 
+$logfile = "C:\Scripts\Office365Update$((Get-Date).ToString('dd-MM-yyyy')).log"
+
+Write-Host "Log file created: $logfile" -ForegroundColor Green
+
+Write-Host "############################################################################################################" -ForegroundColor Green
+Write-Host "Starting the Office 365 update process..." -ForegroundColor Cyan
+
+Add-Content -Path $logfile -Value "Starting the Office 365 update process...$(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+
+if (Test-Path "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe") {
+
+    Write-Host "OfficeC2RClient.exe exists"
+    Write-Host "Let's continue with the update process..." -ForegroundColor Green
+
+    Add-Content -Path $logfile -Value "OfficeC2RClient.exe exists $(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+} else {
+    Write-Host "OfficeC2RClient.exe does not exist"
+
+    Add-Content -Path $logfile -Value "OfficeC2RClient.exe does not exist. $(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+
+    Write-Host "Exiting the update process..." -ForegroundColor Red
+
+    Add-Content -Path $logfile -Value "Exiting the update process...$(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+
+    Write-Host "############################################################################################################" -ForegroundColor Green
+    exit 1
+}
+Write-Host "############################################################################################################" -ForegroundColor Green
+
+Add-Content -Path $logfile -Value "Checking the Office version...$(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+
+Write-Host "Checking the Office version..." -ForegroundColor DarkMagenta
+Write-Host "############################################################################################################" -ForegroundColor Green
+
+Add-Content -Path $logfile -Value "Checking the Office version...$(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+Sleep-Progress 2
 $officeVersion = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration").VersionToReport
+
 Write-Host "Office version: $officeVersion" -ForegroundColor Cyan
+Write-Host "############################################################################################################" -ForegroundColor Green
 
-# Checks if the Office version is already up to date
+Add-Content -Path $logfile -Value "Office version: $officeVersion $(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+Sleep-Progress 2
 
-Sleep-Progress 3
-
-######### Enter newest version to deploy if else then deploy
-if ($officeVersion -eq "16.0.17231.20236") {
+if ($officeVersion -eq $Updateto) {
     Write-Host "Office is already up to date" -ForegroundColor Green
-    Add-Content -Path $logfile -Value "Office is already up to date"
-    Write-Host "Exiting the update process..." -ForegroundColor Cyan
+    Add-Content -Path $logfile -Value "Office is already up to date. $(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
 
-#Message for Monitoring
-    Write-Host $officeVersion -ForegroundColor Green 
-    Add-Content -Path $logfile -Value "Exiting the update process..."
+    Write-Host "Exiting the update process..." -ForegroundColor Cyan
+    Write-Host "############################################################################################################" -ForegroundColor Green
+
+    Add-Content -Path $logfile -Value "Exiting the update process...$(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
     Start-Sleep 2
     exit 0
 }
-#Start Update
-cmd.exe "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user displaylevel=false forceappshutdown=false
 
-Write-Host "Waiting for the update process to finish..." -ForegroundColor Yellow
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "Office 365 update was successful" -ForegroundColor Green
-    Add-Content -Path $logfile -Value "Office 365 update was successful"
-} else {
-    Write-Host "Office 365 update failed" -ForegroundColor Red
-    Add-Content -Path $logfile -Value "Office 365 update failed"
-}
+Write-Host "############################################################################################################" -ForegroundColor Green
+
+Set-Location "C:\Program Files\Common Files\Microsoft Shared\ClickToRun"
+Write-Host "Starting the update process..." -ForegroundColor Cyan
+.\OfficeC2RClient.exe /update user updatetoversion=$Updateto
+Write-Host "Waiting for the update process to finish..." -ForegroundColor Cyan
+Write-Host "############################################################################################################" -ForegroundColor Green
+
+Add-Content -Path $logfile -Value "Waiting for the update process to finish...$(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+Sleep-Progress 600
+
+Write-Host "############################################################################################################" -ForegroundColor Green
+Write-Host "Checking the Office version..." -ForegroundColor Cyan
+Write-Host "############################################################################################################" -ForegroundColor Green
+
+Add-Content -Path $logfile -Value "Checking the Office version...$(Get-Date -format "dd-MM-yyyy ss:mm:HH")"
+Sleep-Progress 2
+$officeVersion = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration").VersionToReport
+
+Write-Host "Office version: $officeVersion" -ForegroundColor Cyan
+Write-Host "############################################################################################################" -ForegroundColor Green
+
+Add-Content -Path $logfile -Value "Office version: $officeVersion $(Get-Date -format "dd-MM-yyyy ss:mm:HH")" 
+exit
